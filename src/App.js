02887fd9243c856152c04d6dc072b437cdbf5e4a
“Ygor <igorgarofalo@gmail.com>
2023-10-29 15:51:11 +0100

    fixes on routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import MyNavbar from "./components/MyNavbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Myheader from "./components/MyHeader";
-import MainContent from "./components/MainContent";
-import MyFooter from "./components/MyFooter";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import AccountPage from "./components/AccountPage";
 
@@ -12,7 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<MyNavbar />} />
-        {/* Altri componenti come <MyHeader /> e <MainContent /> */}
+
         <Route path="/Account" element={<AccountPage />} />
       </Routes>
     </Router>
